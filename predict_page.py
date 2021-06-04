33db4b9b89961ae480e77253166f4e754f809ca5
hhoanguet <hhoanguet@gmail.com>
2021-06-04 12:37:28 +0700

    push project

---
+++ predict_page.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pickle
+import numpy as np
+import os
+import librosa
+from speakerfeatures import extract_features
+from sound import record
+
+
+def load_model():
+    path = "speaker_models\\"
+    gmm_files = [os.path.join(path, file_name) for file_name in os.listdir(path) if file_name.endswith('.gmm')]
+    models = [pickle.load(open(file_namename, 'r+b')) for file_namename in gmm_files]
+    return models
+
+
+models = load_model()
+
+
+def show_predict_page():
+    st.title("Speaker Identification")
+    st.write("### Welcome to the speaker identification demo. Speaker identification can automatically identify the "
+             "person speaking in an audio file given a group of speakers. The input audio is compared against the "
+             " provided group of speakers, and in the case there is a match found. The speaker's identity is returned.")
+    speakers = ["Nguyen Huy Hoang",
+                "Nguyen Manh Dung",
+                "Nguyen Phuc Hai",
+                "Nguyen Xuan Hoang",
+                "Tran Manh Hieu",
+                "Tran Trung Thanh"]
+    uploaded_audio = st.file_uploader("Upload an audio file")
+    if uploaded_audio:
+        st.audio(uploaded_audio)
+        signal, sr = librosa.load(uploaded_audio)
+        X = extract_features(signal, sr)
+    # if st.button("Record"):
+    #     with st.spinner(f'Recording for {6} seconds ....'):
+    #         record()
+    #     st.success("Recording completed")
+    #     st.audio("record.wav")
+    if st.button('Predict'):
+        if not uploaded_audio:
+            signal, sr = librosa.load("record.wav")
+            X = extract_features(signal, sr)
+        log_likelihood = np.zeros(len(models))
+        for i in range(len(models)):
+            gmm = models[i]  # checking with each model one by one
+            scores = np.array(gmm.score(X))
+            log_likelihood[i] = scores.sum()
+        winner = np.argmax(log_likelihood)
+        st.markdown('**' + speakers[winner] + '**' + " is the one identified speaking in the audio.")
---
+++ speakerfeatures.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+@Note :  20 dim MFCC
+         20 dim delta computation on MFCC features. 
+@output : It returns 40 dimensional feature vectors for an audio.
+"""
+
+import numpy as np
+from sklearn import preprocessing
+import librosa
+
+
+def extract_features(signal, sr):
+    """extract 20 dim mfcc features from an audio, performs CMS and combines 
+    delta to make it 40 dim feature vector"""
+
+    mfcc_feature = librosa.feature.mfcc(signal, n_mfcc=20, sr=sr)
+    mfcc_feature = preprocessing.scale(mfcc_feature)
+    mfcc_feature = np.transpose(mfcc_feature)
+    #print(mfcc_feature.shape)
+    delta = librosa.feature.delta(mfcc_feature)
+    #print(delta.shape)
+    combined = np.hstack((mfcc_feature, delta))
+    #print(combined.shape)
+    return combined
+
+
+if __name__ == "__main__":
+     print("In main, Call extract_features(audio,signal_rate) as parameters")
